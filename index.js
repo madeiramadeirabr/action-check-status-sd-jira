d7784574eaec4443235a83cfc295a424bfe118cc
archaic10 <vitor.franca.9400@gmail.com>
2021-11-29 08:23:30 -0300

    feat: release 2

--- index.js
+++ index.js
@@ -3,35 +3,35 @@ const github = require('@actions/github')
 const axios = require('axios')
 
 var status = null
+var typeStatus = null
+var changeStatus = false
 async function run (){    
-    try {
-      if(checkRepoConfig(github.context)){        
-        let basic_auth =  core.getInput("basic-auth")
-        let url = core.getInput("url-jira")    
-        let interval = setInterval(()=>{
-          if(getStatus() == null || getStatus() != 'done')
-            setStatus(basic_auth, url)
-          if (getStatus() != null)
-            checkStatus(interval)
-        } , 30000)
-      }else{
-        core.setFailed("The merge needs to be done with the default branch")
-      }
+    try {        
+      let basic_auth =  core.getInput("basic-auth")
+      let url = core.getInput("url-jira")
+      let otherTypesCurrentStatus = core.getInput("other-types-current-status")        
+      let interval = setInterval(()=>{
+        if(getStatus() == null || getStatus() != 'done')
+          setStatus(basic_auth, url, otherTypesCurrentStatus)
+        if (getStatus() != null)
+          checkStatus(interval)
+      } , 30000)
+      
     } catch (error) {
         core.setFailed(error.message) 
     }
 }
 
 async function checkStatus(interval) {   
-    if (getStatus() == 'done'){
+    if (getStatus() == 'done' || changeStatus ){      
       clearInterval(interval)
-      core.setOutput("result", "Approved Service Desk")
-    }else{
-      console.log('Pending approval')
+      core.setOutput("result", "O Service desk foi aprovado!")
+    }else{      
+      console.log('Pendente de aprovação!')
     }
 }
 
-async function setStatus(basic_auth, url) {
+async function setStatus(basic_auth, url, otherTypesCurrentStatus) {
     
     await axios.get(url,
       {
@@ -41,6 +41,17 @@ async function setStatus(basic_auth, url) {
 
       }).then((res) => {
         status = res.data.currentStatus.statusCategory.toLowerCase()
+        if(otherTypesCurrentStatus){
+          if(typeStatus == null){            
+            setTypeStatus(res.data.currentStatus.status.toLowerCase())
+          }                                 
+
+          if(typeStatus != res.data.currentStatus.status.toLowerCase()){
+            console.log('Mudança de status identificada.')
+            changeStatus = true
+          }            
+          
+        }
       }).catch((error) => {
         core.setFailed(error.message)
       })      
@@ -48,6 +59,14 @@ async function setStatus(basic_auth, url) {
  
 function getStatus() {
     return status
+}
+
+function setTypeStatus(nameStatus){
+    typeStatus = nameStatus
+}
+
+function getTypeStatus(){
+  return typeStatus
 }
 
 function checkRepoConfig (context){
